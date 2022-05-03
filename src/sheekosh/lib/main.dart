f3f423ef3db712117831104f25c71d38426043c8
ideaadegroup <peshawa.k.kamil@gmail.com>
2022-05-03 17:48:26 +0300

    some imporove

--- src/sheekosh/lib/main.dart
+++ src/sheekosh/lib/main.dart
@@ -34,6 +34,7 @@ class MyApp extends StatelessWidget {
         ),
       ],
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
         theme: ThemeData(
           colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.purple)
